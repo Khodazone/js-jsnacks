16770a344e97a9f2b470155c4c6440b6a9d52d86
Khodazone <khodazone@gmail.com>
2023-04-03 20:01:28 +0200

    fix2

---
+++ jsnack3/script.js
@@ -0,0 +1,14 @@
+// Chiedere all'utente 10 numeri diversi
+let number = prompt("Insert a number!")
+
+if(!isNaN(number)){
+for (let number = 0; number < 9; number++) {
+    console.log("Walking east one step");
+  }
+}
+else {
+    console.log("Error")
+    document.getElementById("error").innerHTML = "Please, use only numbers!";
+}
+
+// Sommare i numeri ottenuti
